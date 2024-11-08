1cf4f2deb0d7d0193c789c7b6a60fc16206fab41
Pedro Petrilli <ppetrilli96@gmail.com>
2024-11-08 17:30:44 -0300

    feat: add last transactions component

--- app/data/get-dashboard/index.ts
+++ app/data/get-dashboard/index.ts
@@ -79,6 +79,12 @@ export const getDashBoard = async (month: string) => {
     ),
   }));
 
+  const lastTransactions = await db.transaction.findMany({
+    where,
+    orderBy: { date: "desc" },
+    take: 10,
+  });
+
   return {
     balance,
     depositsTotal,
@@ -86,5 +92,6 @@ export const getDashBoard = async (month: string) => {
     expensesTotal,
     typesPercentage,
     totalExpensePerCategory,
+    lastTransactions,
   };
 };
